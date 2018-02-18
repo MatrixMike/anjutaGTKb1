0428c1aed1bc6276056ea7ec0dd9d001bb4b4dd8
Mike J H <matrixmike@icloud.com>
2018-02-19 00:25:18 +1100

    after indent

--- src/main.c
+++ src/main.c
@@ -28,10 +28,10 @@
 typedef struct _Private Private;
 struct _Private
 {
-	/* ANJUTA: Widgets declaration for gtk_foobar.ui - DO NOT REMOVE */
+  /* ANJUTA: Widgets declaration for gtk_foobar.ui - DO NOT REMOVE */
 };
 
-static Private* priv = NULL;
+static Private *priv = NULL;
 
 /* For testing purpose, define TEST to use the local (not installed) ui file */
 #define TEST
@@ -49,72 +49,70 @@ static Private* priv = NULL;
 
 /* Called when the window is closed */
 void
-destroy (GtkWidget *widget, gpointer data)
+destroy (GtkWidget * widget, gpointer data)
 {
-	gtk_main_quit ();
+  gtk_main_quit ();
 }
 
-static GtkWidget*
+static GtkWidget *
 create_window (void)
 {
-	GtkWidget *window;
-	GtkBuilder *builder;
-	GError* error = NULL;
+  GtkWidget *window;
+  GtkBuilder *builder;
+  GError *error = NULL;
 
-	/* Load UI from file */
-	builder = gtk_builder_new ();
-	if (!gtk_builder_add_from_file (builder, UI_FILE, &error))
-	{
-		g_critical ("Couldn't load builder file: %s", error->message);
-		g_error_free (error);
-	}
+  /* Load UI from file */
+  builder = gtk_builder_new ();
+  if (!gtk_builder_add_from_file (builder, UI_FILE, &error))
+    {
+      g_critical ("Couldn't load builder file: %s", error->message);
+      g_error_free (error);
+    }
 
-	/* Auto-connect signal handlers */
-	gtk_builder_connect_signals (builder, NULL);
+  /* Auto-connect signal handlers */
+  gtk_builder_connect_signals (builder, NULL);
 
-	/* Get the window object from the ui file */
-	window = GTK_WIDGET (gtk_builder_get_object (builder, TOP_WINDOW));
-        if (!window)
-        {
-                g_critical ("Widget \"%s\" is missing in file %s.",
-				TOP_WINDOW,
-				UI_FILE);
-        }
+  /* Get the window object from the ui file */
+  window = GTK_WIDGET (gtk_builder_get_object (builder, TOP_WINDOW));
+  if (!window)
+    {
+      g_critical ("Widget \"%s\" is missing in file %s.",
+		  TOP_WINDOW, UI_FILE);
+    }
 
-	priv = g_malloc (sizeof (struct _Private));
-	/* ANJUTA: Widgets initialization for gtk_foobar.ui - DO NOT REMOVE */
+  priv = g_malloc (sizeof (struct _Private));
+  /* ANJUTA: Widgets initialization for gtk_foobar.ui - DO NOT REMOVE */
 
-	g_object_unref (builder);
+  g_object_unref (builder);
 
-	
-	return window;
+
+  return window;
 }
 
 int
 main (int argc, char *argv[])
 {
- 	GtkWidget *window;
+  GtkWidget *window;
 
 
 
 #ifdef ENABLE_NLS
 
-	bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
-	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
-	textdomain (GETTEXT_PACKAGE);
+  bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
+  bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+  textdomain (GETTEXT_PACKAGE);
 #endif
-	
-	gtk_init (&argc, &argv);
 
-	window = create_window ();
-	gtk_widget_show (window);
+  gtk_init (&argc, &argv);
 
-	gtk_main ();
+  window = create_window ();
+  gtk_widget_show (window);
 
-
-	g_free (priv);
+  gtk_main ();
 
 
-	return 0;
+  g_free (priv);
+
+
+  return 0;
 }
-
